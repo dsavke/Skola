a00d987deea5fae9132959d9b2da715058ad5ca9
savke <dejansavanovic13@gmail.com>
2019-02-05 21:13:18 +0100

    napravljene racunske operacije ovo je samo javascipt sa jquery

---
+++ Skola/Scripts/App/operacija.js
@@ -0,0 +1,28 @@
+﻿$("#btnIzracunaj").click((event) => {
+
+    event.preventDefault();
+
+    var data = {
+        operacija: $("#Operacija:checked").val(),
+        vrijednostPrvogPolja: $("#vrijednost1").val(),
+        vrijednostDrugogPolja: $("#vrijednost2").val()
+    };
+
+    $("#rezultat").val(operacija(data));
+
+    function operacija(data) {
+
+        switch (data.operacija) {
+            case "+":
+                return Number(data.vrijednostPrvogPolja) + Number(data.vrijednostDrugogPolja);
+            case "-":
+                return Number(data.vrijednostPrvogPolja) - Number(data.vrijednostDrugogPolja);
+            case "*":
+                return Number(data.vrijednostPrvogPolja) * Number(data.vrijednostDrugogPolja);
+            case "/":
+                return Number(data.vrijednostPrvogPolja) / Number(data.vrijednostDrugogPolja);
+            default: return 0;
+        }
+    };
+
+});
