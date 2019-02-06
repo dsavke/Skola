7a045fb2557c12845fa850af5b530cbc18d169de
dejan.savanovic <dejansavanovic13@gmail.com>
2019-02-06 11:27:16 +0100

    napravljena igra pogadjanje brojeva

---
+++ Skola/Controllers/IgraController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Skola.Controllers
+{
+    public class IgraController : Controller
+    {
+        // GET: Igra
+        public ActionResult Index()
+        {
+            Random random = new Random();
+            int nasumicanBroj = random.Next(1, 101);
+            ViewBag.NasumicanBroj = nasumicanBroj;
+            return View();
+        }
+    }
+}
---
+++ Skola/Scripts/App/igra.js
@@ -0,0 +1,38 @@
+﻿/*var btn = $(".number");
+
+btn.forEach(ele => {
+    console.log(ele);
+});*/
+
+/*$('.number').each(function () {
+    //if statement here 
+    // use $(this) to reference the current div in the loop
+    //you can try something like...
+
+
+    console.log($(this).children(".vrijednost").val());
+    
+
+});*/
+
+$(".number").click(function (ele) {
+    var pogadjanBroj = $(this).children().html();
+    var nasumicanBroj = $("#vrijednost").html();
+    //console.log($("#vrijednost").html());
+
+    var trenutniBrojPokusaja = $("#txtPokusaji").html();
+    trenutniBrojPokusaja++;
+    $("#txtPokusaji").html(`${trenutniBrojPokusaja}`);
+
+    if (nasumicanBroj < pogadjanBroj) {
+        $("#txtPoruka").html(`Trazeni broj je manji od ${pogadjanBroj}!`)
+        $(this).addClass("nova-boja");
+    } else if (nasumicanBroj > pogadjanBroj) {
+        $("#txtPoruka").html(`Trazeni broj je veci od ${pogadjanBroj}!`);
+        $(this).addClass("nova-boja");
+    } else {
+        $("#txtPoruka").html("Pogodili ste broj!");
+        $(this).addClass("pobjeda");
+    }
+   
+});
